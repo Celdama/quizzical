4b6b7ee1dd058a762cfeccf8fcddb9170c3e97b1
Celdama <celdama.dev@gmail.com>
2022-01-16 08:44:21 +0100

    create GlobalStyle files with styled-components

---
+++ src/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+
+  }
+
+  *{
+    box-sizing: border-box;
+  }
+
+  body{
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+`;

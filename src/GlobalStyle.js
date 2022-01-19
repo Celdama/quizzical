02971423db65bca5c9303e375864a58836359ad2
Celdama <celdama.dev@gmail.com>
2022-01-19 07:37:01 +0100

    GlobalStyle: change background color

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -17,11 +17,12 @@ export const GlobalStyle = createGlobalStyle`
     justify-content: center;
     align-items: center;
     height: 100vh;
-    background-color: #9e2a2b;
+    background-color: #a8dadc;
   }
 
   .App {
     background-color: #fff;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
     /* width: 560px; */
     height: 760px;
     display: flex;
